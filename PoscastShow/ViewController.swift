eddc36d0832e1a1460afe2145a658ef9acf775d9
dangar11 <tkach.igor.vladimirovich@gmail.com>
2019-11-24 13:55:14 +0200

    initializing project

---
+++ PoscastShow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PoscastShow
+//
+//  Created by Igor Tkach on 24.11.2019.
+//  Copyright © 2019 Igor Tkach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
